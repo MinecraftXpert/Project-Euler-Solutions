3752f2abfa83634b84672b0869d16093f2959a1b
NoahR <95318960+MinecraftXpert@users.noreply.github.com>
2023-06-27 14:32:51 -0400

    Add files via upload

--- Project Euler Solutions/Problem 1/index.js
+++ Project Euler Solutions/Problem 1/index.js
@@ -1,24 +1,32 @@
-let sum1 = [];
-let sum2 = [];
+// Finished June 27th 2023
 
+
+
+// Creates an array of multiples of 3
+let multiplesOfThree = [];
+// Creates an array of multiples of 5
+let multiplesOfFive = [];
+
+// Makes a for loop that will run and add multiples of 3 or 5
 for (let i = 1; i < 1000; i++) {
+  // If the remainder is 0, after dividing i / 3, then it's a multiple of 3 and we can push it into the array
   if (i % 3 === 0) {
-    sum1.push(i);
+    multiplesOfThree.push(i);
+    // We have to use else if or it will give us duplicate multiples. But this divides i / 5 and if the remainder is 0, it's a multiple of 5 and we can push it into the array
   } else if (i % 5 === 0) {
-    sum2.push(i);
+    multiplesOfFive.push(i);
   }
 }
 
-const sums1 = sum1.reduce((total, sum) => {
+// Adds up all the numbers in the first array
+const sumOfThrees = multiplesOfThree.reduce((total, sum) => {
   return total + sum;
 });
 
-const sums2 = sum2.reduce((total, sum) => {
+// Adds up all the numbers in the second array
+const sumOfFives = multiplesOfFive.reduce((total, sum) => {
   return total + sum;
 });
 
-console.log(sum1);
-console.log(sum2);
-console.log(sums1);
-console.log(sums2);
-console.log(sums1 + sums2);
+// Adds both numbers and gives us our answer :sunglasses:
+console.log(sumOfThrees + sumOfFives);
